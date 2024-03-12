c1afb3107b8e41aa7990f226829bfabaad825014
WiktorWrosz <wiktorwrosz@o2.pl>
2024-03-12 19:52:13 +0100

    Update project.sh to test webhook in Jenkins

--- project.sh
+++ project.sh
@@ -1,1 +1,2 @@
-echo "This is my Jenkins fullcourse Tutorial. This is my Third Testing" 
+echo "This is my Jenkins fullcourse Tutorial. This is my FIRST Testing of webhook" 
+echo "This is my Jenkins fullcourse Tutorial. This is another line in Testing of webhook"
