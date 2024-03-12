fa35d586abdec0b7ba77cd6c4b51d90f212eabf1
WiktorWrosz <wiktorwrosz@o2.pl>
2024-03-12 19:55:20 +0100

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,2 +1,3 @@
 echo "This is my Jenkins fullcourse Tutorial. This is my FIRST Testing of webhook" 
 echo "This is my Jenkins fullcourse Tutorial. This is another line in Testing of webhook"
+echo "This is my Jenkins fullcourse Tutorial. This is another line in Testing of webhook"
